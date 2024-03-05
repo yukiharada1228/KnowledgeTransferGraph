032771b1eb546081d5e091adc0feee3376844c1e
Yuki Harada <117978472+yukiharada1228@users.noreply.github.com>
2024-03-05 14:47:41 +0900

    Delete ktg directory

--- ktg/models/cifar_models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .cifar_resnet import resnet20, resnet32, resnet56, resnet110
--- ktg/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .checkpoint import load_checkpoint, save_checkpoint
-from .eval import AverageMeter, accuracy
-from .seed import WorkerInitializer, set_seed
