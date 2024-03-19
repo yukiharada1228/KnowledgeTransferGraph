29e7b97d2a4cdbc04895ef945475755d2c54725d
yukiharada1228 <yukiharada1228@gmail.com>
2024-03-19 08:28:33 +0000

    add: commit

--- examples/CIFAR-10/ssl/dcl-train.py
+++ examples/CIFAR-10/ssl/dcl-train.py
@@ -19,11 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--seed", default=42)
 parser.add_argument("--num-nodes", default=3)
 parser.add_argument("--n_trials", default=1500)
-parser.add_argument(
-    "--models", 
-    # default=["resnet18", "resnet34", "resnet50"],
-    default=["resnet18"],
-)
+parser.add_argument("--models", default=["resnet18"])
 parser.add_argument(
     "--gates",
     default=["ThroughGate", "CutoffGate", "PositiveGammaGate", "NegativeGammaGate"],
